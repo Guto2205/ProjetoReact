1ea81d51cd15275fe20ed9e16f2c16b62ae1d4e3
Guto2205 <augustobackups22@gmail.com>
2022-11-18 20:11:30 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,6 +1,9 @@
 import "./styles.css"
 import "../NotificationButton"
 import NotificatioButton from "../NotificationButton"
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 function SalesCard() {
    return (
@@ -8,10 +11,20 @@ function SalesCard() {
          <h2 className="dsmeta-sales-title">Vendas</h2>
          <div>
             <div className="dsmeta-form-control-container">
-               <input className="dsmeta-form-control" type="text" />
+               <DatePicker
+                  selected={new Date()}
+                  onChange={(date: Date) => { }}
+                  className="dsmeta-form-control"
+                  dateFormat="dd/MM/yyyy"
+               />
             </div>
             <div className="dsmeta-form-control-container">
-               <input className="dsmeta-form-control" type="text" />
+               <DatePicker
+                  selected={new Date()}
+                  onChange={(date: Date) => { }}
+                  className="dsmeta-form-control"
+                  dateFormat="dd/MM/yyyy"
+               />
             </div>
          </div>
 
